e11e817425d866466309f421a16eed105f0836aa
Evans-mutuku <evansnyamai98@gmail.com>
2023-01-14 15:36:27 +0300

    prepared ui for edit profile details

--- src/components/UserProfile/EditProfile.js
+++ src/components/UserProfile/EditProfile.js
@@ -10,29 +10,29 @@ import LogoutIcon from "@mui/icons-material/Logout";
 const EditProfile = () => {
   return (
     <main className="pt-20 md:pt-28 mx-wd1 flex justify-between mx-auto">
-      <section className="flex flex-col justify-between mx-h">
+      <section className="flex flex-col justify-between mx-h bg-white py-5 px-8 shadow-md">
         <section>
-          <div className="flex items-center gap-2 mb-3">
+          <div className="flex items-center gap-2 mb-6 cursor-pointer">
             <DashboardIcon className="text-green-400" />
-            <p className="text-xl font-sans">Dashboard</p>
+            <p className="text-md md:text-xl">Dashboard</p>
           </div>
-          <div className="flex items-center gap-2 mb-3">
+          <div className="flex items-center gap-2 mb-6 cursor-pointer">
             <NotificationImportantIcon className="text-yellow-400" />
-            <p className="text-xl font-serif">Notifications</p>
+            <p className="text-md md:text-xl">Notifications</p>
           </div>
-          <div className="flex items-center gap-2 mb-3">
+          <div className="flex items-center gap-2 mb-6 cursor-pointer">
             <SettingsIcon className="text-pink-500" />
-            <p className="text-xl">Settings</p>
+            <p className="text-md md:text-xl">Settings</p>
           </div>
           <div className="flex gap-2 mb-3">
             <ManageAccountsIcon className="text-purple-600" />
             <div>
-              <p className="text-sm md:text-xl">Profile</p>
-              <span className="flex ml-3 mt-2">
+              <p className="text-md md:text-xl">Profile</p>
+              <span className="flex ml-3 mt-2 cursor-pointer">
                 <EditIcon className="w-6 h-2 mr-1 text-green-400" />
                 <p className="">Edit</p>
               </span>
-              <span className="flex ml-3 mt-2">
+              <span className="flex ml-3 mt-2 cursor-pointer">
                 <PreviewIcon className="w-6 h-2 mr-1 text-yellow-600" />
                 <p>View</p>
               </span>
@@ -45,8 +45,200 @@ const EditProfile = () => {
         </div>
       </section>
 
+      <section className="w-full ml-20">
+        <div className="bg-white rounded-md shadow-md px-5 py-4 flex justify-between gap-3 items-center">
+          <h2 className="text-xl">Edit Profile</h2>
+          <span className="flex gap-6">
+            <NotificationImportantIcon />
+            <span>M</span>
+          </span>
+        </div>
 
-      <section></section>
+        <section className="flex gap-7">
+          <div className="mt-5 w-full">
+            <div className="flex flex-col">
+              <label for="" className="font-semibold">
+                Profile Image
+              </label>
+              <input
+                type="file"
+                //   value={tagName}
+                //   onChange={(e) => setTagName(e.target.value)}
+                className="mb-3 mt-1"
+                placeholder="#letsLearn, #javascript"
+              />
+              {/* <p>My Name isb {user?.tagName}</p> */}
+            </div>
+            <div className="flex flex-col">
+              <label for="" className="font-semibold">
+                #TagName
+              </label>
+              <input
+                //   value={tagName}
+                //   onChange={(e) => setTagName(e.target.value)}
+                className="mt h-10 p-3 border border-gray-300 focus:outline-none focus:border-purple-700 rounded mt-1"
+                placeholder="#letsLearn, #javascript"
+              />
+              {/* <p>My Name isb {user?.tagName}</p> */}
+            </div>
+            <div className="flex flex-col mt-5">
+              <label className="font-semibold">Add Website</label>
+              <input
+                //   value={website}
+                //   onChange={(e) => setWebsite(e.target.value)}
+                className="mt h-10 p-3 border border-gray-300 focus:outline-none focus:border-purple-700 rounded mt-1"
+                placeholder="https://example.com"
+              />
+            </div>
+            <div className="flex flex-col mt-5">
+              <label className="font-semibold">Add Location</label>
+              <input
+                //   value={location}
+                //   onChange={(e) => setLocation(e.target.value)}
+                className="mt h-10 p-3 border border-gray-300 focus:outline-none focus:border-purple-700 rounded mt-1"
+                placeholder="Nairobi, Califonia, USA etc"
+              />
+            </div>
+
+            <div className="flex flex-col mt-5">
+              <label for="" className="font-semibold">
+                Work Experience
+              </label>
+              <input
+                //   value={workExperience}
+                //   onChange={(e) => setWorkExperience(e.target.value)}
+                className="mt h-10 p-3 border border-gray-300 focus:outline-none focus:border-purple-700 rounded mt-1"
+                placeholder="Tech Lead,CEO etc.."
+              />
+            </div>
+            <div className="flex flex-col mt-5">
+              <label for="" className="font-semibold">
+                Add Skills
+              </label>
+              <textarea
+                //   value={skills}
+                //   onChange={(e) => setSkills(e.target.value)}
+                className="mt h-18 p-3 pt-1 border border-gray-300 focus:outline-none focus:border-purple-700 rounded mt-1"
+                placeholder="Lawyer, software dev, Content Creator"
+              ></textarea>
+            </div>
+            <div className="flex flex-col mt-5">
+              <label for="" className="font-semibold">
+                Add Bio
+              </label>
+              <textarea
+                //   value={biography}
+                //   onChange={(e) => setBiography(e.target.value)}
+                className="mt h-32 p-3 border border-gray-300 focus:outline-none focus:border-purple-700 rounded mt-1"
+                placeholder="Add your bio"
+              ></textarea>
+            </div>
+
+            <div className="flex justify-between items-center flex-wrap mt-5">
+              <button
+                // onClick={updateProfile}
+                className="bg-c p-3 px-11 text-white rounded-md hover:bg-purple-700 duration-100"
+              >
+                Update Profile
+              </button>
+              <button className="bg-red-600 p-3 text-white rounded-md hover:bg-red-700 duration-100">
+                Delete Account
+              </button>
+            </div>
+          </div>
+          <div className="w-2/6 bg-white mt-5 px-5 py-3 shadow-md ">
+            <p className="font-semibold text-gray-700 mb-3">
+              Choose your Main Interest
+            </p>
+            {/* <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600"> */}
+            <div class="flex items-center pl-3">
+              <input
+                id="vue-checkbox"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              <label
+                for="vue-checkbox"
+                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                All
+              </label>
+            </div>
+            <div class="flex items-center pl-3">
+              <input
+                id="vue-checkbox"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              <label
+                for="vue-checkbox"
+                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Art
+              </label>
+            </div>
+            <div class="flex items-center pl-3">
+              <input
+                id="vue-checkbox"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              <label
+                for="vue-checkbox"
+                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Programming
+              </label>
+            </div>
+            <div class="flex items-center pl-3">
+              <input
+                id="vue-checkbox"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              <label
+                for="vue-checkbox"
+                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Health
+              </label>
+            </div>
+            <div class="flex items-center pl-3">
+              <input
+                id="vue-checkbox"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              <label
+                for="vue-checkbox"
+                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Business
+              </label>
+            </div>
+            <div class="flex items-center pl-3">
+              <input
+                id="vue-checkbox"
+                type="checkbox"
+                value=""
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              />
+              <label
+                for="vue-checkbox"
+                class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Vue JS
+              </label>
+            </div>
+            {/* </li> */}
+          </div>
+        </section>
+      </section>
     </main>
   );
 };

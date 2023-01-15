deb9ceb58694e107c40f8f0e5a0d2ffa26e2fce5
Evans-mutuku <evansnyamai98@gmail.com>
2023-01-15 16:42:00 +0300

    displayed personal profile

--- src/components/UserProfile/ProfilePreview.js
+++ src/components/UserProfile/ProfilePreview.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import NotificationImportantIcon from "@mui/icons-material/NotificationImportant";
 import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
@@ -9,9 +9,32 @@ import LogoutIcon from "@mui/icons-material/Logout";
 import AnalyticsIcon from "@mui/icons-material/Analytics";
 import ContactSupportIcon from "@mui/icons-material/ContactSupport";
 import { useNavigate } from "react-router-dom";
+import { db } from "../../utils/firebase";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/userSlice";
 
 const ProfilePreview = () => {
   const navigate = useNavigate();
+  const user = useSelector(selectUser);
+  const [userProfileDetails, setUserProfileDetails] = useState([]);
+
+  const fetchUserProfileDetails = async () => {
+    await db
+      .collection("Users")
+      .where("uid", "==", user?.uid)
+      .onSnapshot((snapshot) => {
+        setUserProfileDetails(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }))
+        );
+      });
+  };
+  useEffect(() => {
+    fetchUserProfileDetails();
+  }, []);
+
   return (
     <main className="pt-20 md:pt-28 mx-wd1 flex justify-between mx-auto">
       <section className="flex flex-col justify-between mx-h bg-white py-5 px-8 shadow-md">
@@ -78,52 +101,69 @@ const ProfilePreview = () => {
       </section>
 
       <section className="w-full ml-20 bg-white">
-        <article className="flex items-top">
-          <img
-            className="rounded-md w-48 h-32"
-            src="https://media.licdn.com/dms/image/C5603AQHorwJKFNaR3Q/profile-displayphoto-shrink_800_800/0/1604525502427?e=1678924800&v=beta&t=GyjN_4oha_XLHHeaQ3lGRM9RRc4KELCBoj2mcEcRhuE"
-            alt=""
-          />
+        {userProfileDetails &&
+          userProfileDetails.map((userData) => {
+            return (
+              <>
+                {/* <div className="blog-container">
+                  <h4>{userData.location}</h4>
+                  <p>{userData.skills}</p>
+                </div> */}
 
-          <div className="ml-12">
-            <h1 className="text-md md:text-2xl">Evans Mutuku</h1>
-            <p className="font-semibold">Founder - Melbite</p>
-            <p className="text-purple-600 font-semibold mt-4">
-              #JavascriptDaddie
-            </p>
-          </div>
-        </article>
-        <article className="mt-16 w-4/6 p-2">
-          <h1>About</h1>
-          <div className="flex justify-between gap-28 mb-4 mt-3">
-            <div className="flex flex-col gap-6 mb-4">
-              <span className="font-semibold">User ID</span>
-              <span className="font-semibold">Name</span>
-              <span className="font-semibold">Email</span>
-              <span className="font-semibold">Website</span>
-              <span className="font-semibold">Location</span>
-              <span className="font-semibold">Work</span>
-              <span className="font-semibold">Skills</span>
-              <span className="font-semibold">Bio</span>
-            </div>
+                <article className="flex items-top">
+                  <img
+                    className="rounded-md w-48 h-32"
+                    src={userData.profileImage}
+                    alt=""
+                  />
 
-            <div className="flex flex-col gap-6 mb-4">
-              <p>JTVSv390jv03J300Pod</p>
-              <p>Evans Mutuku</p>
-              <p>evansnyamai98@gmail.com</p>
-              <a href="https://melbite.com" className="text-purple-500 underlined" target="_blank">https://melbite.com</a>
-              <p>Nairobi - Kenya</p>
-              <p>Founder - Melbite</p>
-              <p>Javascript, React, NodeJs, NextJs, Tailwind, Typescript</p>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
-                natus tempora vero sunt dolor voluptatum, fugiat cum? Quaerat
-                aperiam sequi veniam sunt, officiis quo mollitia velit tenetur,
-                quis minima veritatis.
-              </p>
-            </div>
-          </div>
-        </article>
+                  <div className="ml-12">
+                    <h1 className="text-md md:text-2xl">{user.displayName}</h1>
+                    <p className="font-semibold">{userData?.workExperience}</p>
+                    <p className="text-purple-600 font-semibold mt-4 flex items-center">#
+                    {userData?.tagName}
+                    </p>
+                  </div>
+                </article>
+                <article className="mt-16 w-4/6 p-2">
+                  <h1>About</h1>
+                  <div className="flex justify-between gap-28 mb-4 mt-3">
+                    <div className="flex flex-col gap-6 mb-4">
+                      <span className="font-semibold">User ID</span>
+                      <span className="font-semibold">Name</span>
+                      {/* <span className="font-semibold">Email</span> */}
+                      <span className="font-semibold">Website</span>
+                      <span className="font-semibold">Location</span>
+                      <span className="font-semibold">Work</span>
+                      <span className="font-semibold">Skills</span>
+                      <span className="font-semibold">Bio</span>
+                    </div>
+
+                    <div className="flex flex-col gap-6 mb-4">
+                      <p>{userData.uid}</p>
+                      <p>{user.displayName}</p>
+                      {/* <p>{userData.emailAddress}</p> */}
+                      <a
+                        href="https://melbite.com"
+                        className="text-purple-500 underlined"
+                        target="_blank"
+                      >
+                        {userData.website}
+                      </a>
+                      <p>{userData.location}</p>
+                      <p>{userData.workExperience}</p>
+                      <p>
+                        {userData.skills}
+                      </p>
+                      <p>
+                      {userData.biography}
+                      </p>
+                    </div>
+                  </div>
+                </article>
+              </>
+            );
+          })}
       </section>
     </main>
   );

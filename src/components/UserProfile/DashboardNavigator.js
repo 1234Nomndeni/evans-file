64c736fc1e22bf72d3e7b03cf5f80d9b4a3daa17
Evans-mutuku <evansnyamai98@gmail.com>
2023-01-18 17:28:46 +0300

    add dashboard navigator details

--- src/components/UserProfile/DashboardNavigator.js
+++ src/components/UserProfile/DashboardNavigator.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const DashboardNavigator = () => {
+    const navigate = useNavigate()
   return (
-    <div>DashboardNavigator</div>
-  )
-}
+    <main>
+      <section className="hidden md:flex flex-col justify-between mx-h bg-white py-5 px-8 shadow-md">
+        <section>
+          <div
+            onClick={() => navigate("/dashboard")}
+            className="flex items-center gap-2 mb-6 cursor-pointer"
+          >
+            <DashboardIcon className="text-green-400" />
+            <p className="text-md md:text-xl">Dashboard</p>
+          </div>
+          <div
+            onClick={() => navigate("/notifications")}
+            className="flex items-center gap-2 mb-6 cursor-pointer"
+          >
+            <NotificationImportantIcon className="text-yellow-400" />
+            <p className="text-md md:text-xl">Notifications</p>
+          </div>
+          <div
+            onClick={() => navigate("")}
+            className="flex items-center gap-2 mb-6 cursor-pointer"
+          >
+            <SettingsIcon className="text-pink-500" />
+            <p className="text-md md:text-xl">Settings</p>
+          </div>
+          <div
+            onClick={() => navigate("")}
+            className="flex items-center gap-2 mb-6 cursor-pointer"
+          >
+            <AnalyticsIcon className="text-blue-500" />
+            <p className="text-md md:text-xl">Analytics</p>
+          </div>
+          <div className="flex gap-2 mb-3">
+            <ManageAccountsIcon className="text-purple-600" />
+            <div>
+              <p className="text-md md:text-xl">Profile</p>
+              <span
+                onClick={() => navigate("/editprofile")}
+                className="flex ml-2 mt-2 cursor-pointer"
+              >
+                <EditIcon className="w-6 h-2 mr-1 text-green-400" />
+                <p className="">Edit</p>
+              </span>
+              <span
+                onClick={() => navigate("/previewprofile")}
+                className="flex ml-2 mt-2 cursor-pointer"
+              >
+                <PreviewIcon className="w-6 h-2 mr-1 text-yellow-600" />
+                <p>View</p>
+              </span>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div
+            onClick={() => navigate("/contact-us")}
+            className="flex items-between gap-2 cursor-pointer mb-4 font-semibold"
+          >
+            <ContactSupportIcon className="text-green-500" />
+            <p>Support</p>
+          </div>
+          <div
+            onClick={signOutOfApp}
+            className="flex items-between gap-2 cursor-pointer mb-4 font-semibold"
+          >
+            <LogoutIcon className="text-purple-600" />
+            <p className="">Log Out</p>
+          </div>
+        </section>
+      </section>
+    </main>
+  );
+};
 
-export default DashboardNavigator
+export default DashboardNavigator;

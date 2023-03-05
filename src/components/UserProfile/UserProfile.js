a48fddbaf8b1ae76703f811b28f36d0e64411204
Evans-mutuku <evansnyamai98@gmail.com>
2023-03-05 22:06:00 +0300

    adding user posts

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -7,7 +7,13 @@ import AddIcon from "@mui/icons-material/Add";
 import DoneIcon from "@mui/icons-material/Done";
 import LanguageIcon from "@mui/icons-material/Language";
 import EditLocationIcon from "@mui/icons-material/EditLocation";
+import { HeartIcon, ChatIcon, ShareIcon } from "@heroicons/react/outline";
+import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 function Article() {
   const { name_slug, displayName } = useParams();
@@ -101,11 +107,11 @@ function Article() {
                   <h2 className="text-xl">{userProfile.displayName}</h2>
                   <p className="mb-1">{userProfile.workExperience}</p>
                   <span className="flex gap-2 mb-1">
-                    <EditLocationIcon />
+                    <EditLocationIcon className="text-gray-500" />
                     <p>{userProfile.location}</p>
                   </span>
                   <span className="flex gap-2">
-                    <LanguageIcon />
+                    <LanguageIcon className="text-gray-500" />
                     <a
                       className="text-purple-900"
                       href={userProfile.website}
@@ -135,7 +141,7 @@ function Article() {
                   )}
                 </button>
                 <p>
-                  <p className="mb-2 text-gray-600 font-semibold">
+                  <p className="mt-3 text-gray-600">
                     {followers.length} Followers
                   </p>
                 </p>
@@ -151,24 +157,177 @@ function Article() {
         )}
       </section>
 
-      <h1>Posts</h1>
+      <section className="mx-auto max-w-7xl mt-3">
+        <h1>Posts</h1>
+        <article className="w-full">
+          {userPosts &&
+            userPosts.map((post) => (
+              <article
+                className="w-full border-2 rounded-md bg-white p-5 mb-2 hover:border-purple-800 duration-150"
+                key={post.id}
+              >
+                <section className="flex items-center ">
+                  <span className="bg-yellow-300 w-10 font-mono p-1 pl-3 uppercase text-xl text-gray-800 h-10 border-2 border-yellow-300 rounded-full">
+                    {post.displayName?.[0]}
+                  </span>
+                  <span className="ml-2">
+                    <h3 className="text-sm">{post.displayName}</h3>
+                    <p className="text-sm text-gray-500 -mt-1">
+                      Published{" "}
+                      <ReactTimeago
+                        date={new Date(post.timestamp?.toDate()).toUTCString()}
+                      />
+                    </p>
+                  </span>
+                </section>
+
+                <section className="mt-2">
+                  <Link to={`/${post.name_slug}/${post.id}`}>
+                    <h1 className="md:leading-9 text-lg md:text-2xl text-gray-900 hover:text-purple-900 cursor-pointer">
+                      {post.blogHeader}{" "}
+                    </h1>
+                  </Link>
+                </section>
+                <section className="flex gap-1 text-xs md:text-sm md:flex md:gap-3 mt-4 flex-wrap w-full">
+                  {post.hashTags?.map((tag) => (
+                    <Link
+                      key={tag}
+                      to={`/tags/${tag}`}
+                      className="rounded-md max-w-min bg-green-50 hover:bg-green-100 py-1 px-2 border cursor-pointer"
+                    >
+                      #{tag}
+                    </Link>
+                  ))}
+                </section>
+                <section className="flex justify-between">
+                  <span className="flex items-center w-2/5 gap-3 justify-between text-gray-400">
+                    <Link
+                      to={`/${post.name_slug}/${post.id}`}
+                      className="flex items-center space-x-1 hover:bg-gray-200 p-2 rounded-md cursor-pointer"
+                    >
+                      <HeartIcon className="w-6 cursor-pointer " />
+                      <p className="text-sm text-gray-600">
+                        {post.likes?.length}
+                      </p>
+                      <p className="text-sm text-gray-600">Likes</p>
+                    </Link>
+
+                    <Link
+                      to={`/${post.name_slug}/${post.id}`}
+                      className="flex items-center hover:bg-gray-200 p-2 rounded-md cursor-pointer"
+                    >
+                      <ChatIcon className="w-6 cursor-pointer text-gray-500 " />
+                      <p className="hidden sm:block ml-2 text-sm text-gray-600">
+                        Comment
+                        {posts.commentCount}
+                      </p>
+                    </Link>
+                  </span>
+                  <Menu as="div" className="ml-3 relative">
+                    <div>
+                      <Menu.Button className="flex items-center hover:bg-gray-200 p-2 rounded-md cursor-pointer">
+                        <ShareIcon className="h-7 cursor-pointer rounded-full p-1 text-gray-500 " />
+                        <p className="hidden sm:block ml-2 text-sm text-gray-600">
+                          Share
+                        </p>
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href={`https://twitter.com/intent/tweet?url=https://melbite.com/${post.name_slug}/${post.id}`}
+                              target="_blank"
+                              rel="noreferrer"
+                              className={classNames(
+                                active ? "bg-white" : "",
+                                "block px-4 py-2 text-sm text-gray-700 hover:text-purple-900"
+                              )}
+                            >
+                              Share on Twitter
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href={`https://www.facebook.com/sharer.php?u=https://melbite.com/${post.name_slug}/${post.id}`}
+                              target="_blank"
+                              rel="noreferrer"
+                              className={classNames(
+                                active ? "bg-white" : "",
+                                "block px-4 py-2 text-sm text-gray-700 hover:text-purple-900"
+                              )}
+                            >
+                              Share on Facebook
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href={`https://www.linkedin.com/sharing/share-offsite/?url=https://melbite.com/${post.name_slug}/${post.id}`}
+                              target="_blank"
+                              rel="noreferrer"
+                              className={classNames(
+                                active ? "bg-white" : "",
+                                "block px-4 py-2 text-sm text-gray-700 hover:text-purple-900"
+                              )}
+                            >
+                              Share on LinkedIn
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href={`https://www.reddit.com/submit?url=https://melbite.com/${post.name_slug}/${post.id}`}
+                              target="_blank"
+                              rel="noreferrer"
+                              className={classNames(
+                                active ? "bg-white" : "",
+                                "block px-4 py-2 text-sm text-gray-700 hover:text-purple-900"
+                              )}
+                            >
+                              Share on Reddit
+                            </a>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </section>
+              </article>
+            ))}
+        </article>
+      </section>
+
       <h3>{userPosts.length} Posts</h3>
-      <div>
+      {/* <div>
         {userPosts &&
           userPosts?.map((post) => (
             <li key={post.id}>
               <h4>{post.blogHeader}</h4>
               <p>{post.content}</p>
-              {/* <ul>
+              <ul>
                 {post.tags.map((tag) => (
                   <li key={tag} className="tag">
                     {tag}
                   </li>
                 ))}
-              </ul> */}
+              </ul>
             </li>
           ))}
-      </div>
+      </div> */}
     </main>
   );
 }

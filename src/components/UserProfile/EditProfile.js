12a61d218c51896d2a84c8881e1a7251b186e163
Evans-mutuku <evansnyamai98@gmail.com>
2023-03-16 13:09:13 +0300

    removed duplicates

--- src/components/UserProfile/EditProfile.js
+++ src/components/UserProfile/EditProfile.js
@@ -1,20 +1,13 @@
 import { useState, useEffect } from "react";
-import DashboardIcon from "@mui/icons-material/Dashboard";
 import NotificationImportantIcon from "@mui/icons-material/NotificationImportant";
-import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
-import SettingsIcon from "@mui/icons-material/Settings";
-import EditIcon from "@mui/icons-material/Edit";
-import PreviewIcon from "@mui/icons-material/Preview";
-import LogoutIcon from "@mui/icons-material/Logout";
-import AnalyticsIcon from "@mui/icons-material/Analytics";
-import ContactSupportIcon from "@mui/icons-material/ContactSupport";
-import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { logout, selectUser } from "../../features/userSlice";
-import { auth, db, storage } from "../../utils/firebase";
+
+import { useSelector } from "react-redux";
+import { selectUser } from "../../features/userSlice";
+import { db, storage } from "../../utils/firebase";
 import "react-toastify/dist/ReactToastify.css";
 import { toast } from "react-toastify";
 import DashboardNavigator from "./DashboardNavigator";
+import DashboardLinks from "./DashboardLinks";
 
 toast.configure({
   position: toast.POSITION.TOP_CENTER,
@@ -31,8 +24,6 @@ toast.configure({
 });
 
 const EditProfile = ({ uid, editLocation }) => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
   const user = useSelector(selectUser);
   const [profilePic, setProfilePic] = useState(null);
   const [uploadedImage, setUploadedImage] = useState("");
@@ -132,89 +123,13 @@ const EditProfile = ({ uid, editLocation }) => {
     fetchUserProfileDetails();
   }, []);
 
-  const signOutOfApp = () => {
-    dispatch(logout);
-    auth.signOut();
-    window.location.reload(false);
-
-    if (user) {
-      auth.signOut();
-    }
-    navigate("/");
-  };
-
   return (
     <main className="md:pt-28 mx-wd1 flex justify-between md:flex-row flex-col mx-auto">
       <div className="block md:hidden">
         <DashboardNavigator />
       </div>
-      <section className="hidden md:flex flex-col justify-between mx-h bg-white py-5 px-8 shadow-md">
-        <section>
-          <div
-            onClick={() => navigate("/dashboard")}
-            className="flex items-center gap-2 mb-6 cursor-pointer"
-          >
-            <DashboardIcon className="text-green-400" />
-            <p className="text-md md:text-xl">Dashboard</p>
-          </div>
-          <div
-            onClick={() => navigate("/notifications")}
-            className="flex items-center gap-2 mb-6 cursor-pointer"
-          >
-            <NotificationImportantIcon className="text-yellow-400" />
-            <p className="text-md md:text-xl">Notifications</p>
-          </div>
-          <div
-            onClick={() => navigate("")}
-            className="flex items-center gap-2 mb-6 cursor-pointer"
-          >
-            <SettingsIcon className="text-pink-500" />
-            <p className="text-md md:text-xl">Settings</p>
-          </div>
-          <div
-            onClick={() => navigate("")}
-            className="flex items-center gap-2 mb-6 cursor-pointer"
-          >
-            <AnalyticsIcon className="text-blue-500" />
-            <p className="text-md md:text-xl">Analytics</p>
-          </div>
-          <div className="flex gap-2 mb-3">
-            <ManageAccountsIcon className="text-purple-600" />
-            <div>
-              <p className="text-md md:text-xl">Profile</p>
-              <span
-                onClick={() => navigate("/editprofile")}
-                className="flex ml-2 mt-2 cursor-pointer"
-              >
-                <EditIcon className="w-6 h-2 mr-1 text-green-400" />
-                <p className="">Edit</p>
-              </span>
-              <span
-                onClick={() => navigate("/previewprofile")}
-                className="flex ml-2 mt-2 cursor-pointer"
-              >
-                <PreviewIcon className="w-6 h-2 mr-1 text-yellow-600" />
-                <p>View</p>
-              </span>
-            </div>
-          </div>
-        </section>
-        <section>
-          <div
-            onClick={() => navigate("/contact-us")}
-            className="flex items-between gap-2 cursor-pointer mb-4 font-semibold"
-          >
-            <ContactSupportIcon className="text-green-500" />
-            <p>Support</p>
-          </div>
-          <div
-            onClick={signOutOfApp}
-            className="flex items-between gap-2 cursor-pointer mb-4 font-semibold"
-          >
-            <LogoutIcon className="text-purple-600" />
-            <p className="">Log Out</p>
-          </div>
-        </section>
+      <section>
+        <DashboardLinks />
       </section>
 
       <section className="w-full ml-0 md:ml-20">
--- src/components/UserProfile/UserDashboard.js
+++ src/components/UserProfile/UserDashboard.js
@@ -1,8 +1,8 @@
 import { useEffect, useState, Fragment } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import { logout, selectUser } from "../../features/userSlice";
-import { auth, db } from "../../utils/firebase";
+import { selectUser } from "../../features/userSlice";
+import { db } from "../../utils/firebase";
 import { Menu, Transition } from "@headlessui/react";
 import { Helmet } from "react-helmet";
 import {
@@ -13,27 +13,19 @@ import {
   EyeIcon,
   DocumentAddIcon,
 } from "@heroicons/react/outline";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import NotificationImportantIcon from "@mui/icons-material/NotificationImportant";
-import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
-import SettingsIcon from "@mui/icons-material/Settings";
-import EditIcon from "@mui/icons-material/Edit";
-import PreviewIcon from "@mui/icons-material/Preview";
-import LogoutIcon from "@mui/icons-material/Logout";
-import AnalyticsIcon from "@mui/icons-material/Analytics";
-import ContactSupportIcon from "@mui/icons-material/ContactSupport";
+
 import ReactTimeago from "react-timeago";
 import DeleteMyArticle from "./DeleteMyArticle";
 import EditMyArticle from "./EditMyArticle";
 import DashboardNavigator from "./DashboardNavigator";
+import DashboardLinks from "./DashboardLinks";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-const UserDashboard = ({ name_slug, open, setOpen }) => {
+const UserDashboard = ({ name_slug }) => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
   const [userPosts, setUserPosts] = useState([]);
   const user = useSelector(selectUser);
   const [articlesCount, setArticlesCount] = useState(0);
@@ -65,97 +57,18 @@ const UserDashboard = ({ name_slug, open, setOpen }) => {
     fetchArticleCount();
   }, []);
 
-  const signOutOfApp = () => {
-    dispatch(logout);
-    auth.signOut();
-    window.location.reload(false);
-
-    if (user) {
-      auth.signOut();
-    }
-    navigate("/");
-  };
-
   return (
     <main className="md:pt-28 mx-wd1 flex justify-between md:flex-row flex-col mx-auto">
       <Helmet>
         <title>Melbite | Dashboard</title>
       </Helmet>
 
-      {/* <section className="flex"> */}
-      {/* <section> */}
       <div className="block md:hidden">
         <DashboardNavigator />
       </div>
-      <section className="hidden md:flex flex-col justify-between mx-h bg-white py-5 px-8 shadow-md">
-        <section>
-          <div
-            onClick={() => navigate("/dashboard")}
-            className="flex items-center gap-2 mb-6 cursor-pointer"
-          >
-            <DashboardIcon className="text-green-400" />
-            <p className="text-md md:text-xl">Dashboard</p>
-          </div>
-          <div
-            onClick={() => navigate("/notifications")}
-            className="flex items-center gap-2 mb-6 cursor-pointer"
-          >
-            <NotificationImportantIcon className="text-yellow-400" />
-            <p className="text-md md:text-xl">Notifications</p>
-          </div>
-          <div
-            onClick={() => navigate("")}
-            className="flex items-center gap-2 mb-6 cursor-pointer"
-          >
-            <SettingsIcon className="text-pink-500" />
-            <p className="text-md md:text-xl">Settings</p>
-          </div>
-          <div
-            onClick={() => navigate("")}
-            className="flex items-center gap-2 mb-6 cursor-pointer"
-          >
-            <AnalyticsIcon className="text-blue-500" />
-            <p className="text-md md:text-xl">Analytics</p>
-          </div>
-          <div className="flex gap-2 mb-3">
-            <ManageAccountsIcon className="text-purple-600" />
-            <div>
-              <p className="text-md md:text-xl">Profile</p>
-              <span
-                onClick={() => navigate("/editprofile")}
-                className="flex ml-2 mt-2 cursor-pointer"
-              >
-                <EditIcon className="w-6 h-2 mr-1 text-green-400" />
-                <p className="">Edit</p>
-              </span>
-              <span
-                onClick={() => navigate("/previewprofile")}
-                className="flex ml-2 mt-2 cursor-pointer"
-              >
-                <PreviewIcon className="w-6 h-2 mr-1 text-yellow-600" />
-                <p>View</p>
-              </span>
-            </div>
-          </div>
-        </section>
-        <section>
-          <div
-            onClick={() => navigate("/contact-us")}
-            className="flex items-between gap-2 cursor-pointer mb-4 font-semibold"
-          >
-            <ContactSupportIcon className="text-green-500" />
-            <p>Support</p>
-          </div>
-          <div
-            onClick={signOutOfApp}
-            className="flex items-between gap-2 cursor-pointer mb-4 font-semibold"
-          >
-            <LogoutIcon className="text-purple-600" />
-            <p className="">Log Out</p>
-          </div>
-        </section>
+      <section>
+        <DashboardLinks />
       </section>
-      {/* </section> */}
 
       <section className="ml-0 md:ml-20 w-full">
         <section className="mb-3">
